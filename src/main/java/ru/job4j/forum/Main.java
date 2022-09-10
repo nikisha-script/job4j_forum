009db50e5293d28c1811e15dd9bda0eb11632456
nikisha-script <nikisha_script@mail.ru>
2022-09-10 11:00:14 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/forum/Main.java
@@ -0,0 +1,14 @@
+package ru.job4j.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("run to: http://localhost:8189/index");
+    }
+
+}
